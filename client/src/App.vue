dd5394eadffc18babc8c5eb484574a824e18a6fc
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-31 10:41:59 +0000

    imported API countries and tested

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>{{countries[0].name}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data(){
+        return{
+        countries: []
+        }
+    },
+
+    mounted(){
+        fetch('https://restcountries.eu/rest/v2/all') // API
+            .then(res => res.json())
+            .then((countries) => (this.countries = countries))
+    }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
