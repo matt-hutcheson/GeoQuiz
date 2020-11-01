39202faace401836235f017f85153e4225610928
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-01 18:44:35 +0000

    added logic to check user input and move to next flag

--- client/src/components/Play/playArticle.vue
+++ client/src/components/Play/playArticle.vue
@@ -1,12 +1,14 @@
 <template>
 <section>
     <h1>You are now in Play mode.</h1>
-    <flag-to-guess v-if="randomCountry" :randomCountry="randomCountry"></flag-to-guess>
-    <select name="flagCountry" id="">
-        <option selected disabled value="">--Select A Country--</option>
-        <option v-for="(country, alpha3Code) in countriesRemaining" :key="alpha3Code" value="">{{ country.name }}</option>
-    </select>
     <button v-if="randomCountry" v-on:click.prevent="getRandomCountry(countriesRemaining)">Change Flag</button>
+    <flag-to-guess v-if="randomCountry" :randomCountry="randomCountry"></flag-to-guess>
+        <select v-if="countriesRemaining" @change="checkAnswer()" name="flagCountry" id="" v-model="countryListSelected">
+            <option selected disabled value="">--Select A Country--</option>
+            <option v-for="(country, alpha3Code) in countriesRemaining" :key="alpha3Code" :value="country">{{ country.name }}</option>
+
+        </select>
+
     <play-map :countries="countries"></play-map>
 </section>
 </template>
@@ -27,14 +29,29 @@ export default {
         currentFlag: null,
         randomCountry: null,
         countriesRemaining: [],
-        countriesCorrect: []
+        countriesCorrect: [],
+        countryListSelected: null
         }
     },
     methods: {
         getRandomCountry (array) {
-        this.randomCountry = array[Math.floor(Math.random() * array.length)]
-        }
+            this.randomCountry = array[Math.floor(Math.random() * array.length)]
+            },
+
+        checkAnswer () {
+            console.log(this.countryListSelected.alpha3Code)
+            if (this.randomCountry.alpha3Code === this.countryListSelected.alpha3Code) {
+                this.countriesCorrect.push(this.countryListSelected)
+                const index = this.countriesRemaining.indexOf(this.countryListSelected)
+                this.countriesRemaining.splice(index, 1)
+                this.getRandomCountry(this.countriesRemaining)
+                this.countryListSelected = null
+            }
+    }
+
     },
+
+
     mounted() {
         this.countriesRemaining = this.countries
         this.getRandomCountry(this.countriesRemaining)
