f8b0271799d01994b4c42825da3cc5368ca50ae7
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-01 13:41:02 +0000

    added random flag to page

---
+++ client/src/components/Play/flagToGuess.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <div class="flag-image" :style=" { backgroundImage: 'url(' + randomCountry.flag + ')'}"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "flag-to-guess",
+  props: ["countries"],
+  data () {
+    return {
+      currentFlag: null,
+      randomCountry: null,
+      countriesRemaining: [],
+      contriesCorrect: []
+    }
+  },
+  mounted() {
+    this.countriesRemaining = this.countries
+    this.randomiseList(this.countriesRemaining)
+  },
+  methods: {
+    randomiseList (array) {
+      this.randomCountry = array[Math.floor(Math.random() * array.length)]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.flag-image {
+  width: 100px;
+  height: 100px;
+  background-size: cover;
+}
+
+</style>
--- client/src/components/Play/playArticle.vue
+++ client/src/components/Play/playArticle.vue
@@ -1,18 +1,21 @@
 <template>
 <section>
     <h1>You are now in Play mode.</h1>
+    <flag-to-guess v-if="countries.length" :countries="countries"></flag-to-guess>
     <play-map :countries="countries"></play-map>
 </section>
 </template>
 
 <script>
 import playMap from './playMap';
+import flagToGuess from './flagToGuess';
 
 export default {
     name: 'playArticle',
     props: ['currentMode', 'countries'],
     components: {
-        'play-map': playMap
+        'play-map': playMap,
+        'flag-to-guess': flagToGuess
     }
 
 }
