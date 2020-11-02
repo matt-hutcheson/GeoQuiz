ae0fcffaf1a3d2e0fd753b7cf064139fb09c5de0
CanadiAnn <annie.lozeau@hotmail.com>
2020-11-02 15:53:27 +0000

    setup server structure

---
+++ server/db/seeds_data.js
@@ -0,0 +1,25 @@
+const { Db } = require("mongodb");
+
+use geoquiz;
+db.dropDatabase();
+
+db.results.insertMany([
+{
+    user_name: 'user_name',
+    countries: {
+        
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+])
