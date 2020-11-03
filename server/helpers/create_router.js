b917210641d35bc5e6a37221a924a777ddc8236a
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-03 14:36:00 +0000

    added update and delete routes but still to test

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -46,6 +46,37 @@ const createRouter = function (collection) {
     });
   });
 
+  // UPDATE
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    delete updatedData._id;
+
+    collection
+    .findOneAndUpdate({ _id: ObjectID(id) }, { $set: updatedData })
+    .then(result => {
+      res.json(result.value);
+    })
+    .catch((err) => {
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  // DELETE
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne({ _id: ObjectID(id) })
+    .then(result => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
   return router;
 };
 module.exports = createRouter;
