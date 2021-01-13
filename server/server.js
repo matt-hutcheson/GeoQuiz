67a942dbd5204825345289bcea5eacdf318b31e5
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2021-01-13 09:52:48 +0000

    setup .env

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,12 +6,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
-const server_url = require('./helpers/server_key');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-MongoClient.connect(server_url, { useUnifiedTopology: true})
+MongoClient.connect(process.env.SERVER_URL, { useUnifiedTopology: true})
     .then((client) => {
         const db = client.db('geoquiz');
         const resultsCollection = db.collection('results');
@@ -25,6 +24,6 @@ MongoClient.connect(server_url, { useUnifiedTopology: true})
         app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
     }
 
-    app.listen(PORT, function() {
+    app.listen(port, function() {
         console.log(`Listening on port ${ this.address().port }`);
     });
