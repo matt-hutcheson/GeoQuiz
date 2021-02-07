59af3f940e408b330090826ebc03b0f03f268555
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2021-02-07 13:51:59 +0000

    added hints section

--- client/src/components/Play/mapHeader.vue
+++ client/src/components/Play/mapHeader.vue
@@ -4,9 +4,17 @@
       <button v-if="randomCountry" v-on:click.prevent="getRandomCountry(countriesRemaining)">Change Flag</button>
     </div>
     <flag-to-guess v-if="randomCountry" :randomCountry="randomCountry"></flag-to-guess>
-    <section id="flag-results">
-      <p v-if="result==='correct'" class="result-correct"><font-awesome-icon :icon="['fas', 'check-circle']"/></p>
-      <p v-if="result==='incorrect'" class="result-wrong"><font-awesome-icon :icon="['fas', 'times-circle']"/></p>
+    <section id="hints-container">
+      <div id="hints-header-container">
+        <h2 id="hints-header">Hints</h2>
+        <button v-on:click.prevent="getHint()" id="hint-button">Get Hint</button>
+      </div>
+      <p v-if="hints > 0" class="hint"> 1. Its capital is: {{ randomCountry.capital }}</p>
+      <p v-if="hints > 1" class="hint"> 2. It is found in: {{ randomCountry.region }}</p>
+      <p v-if="hints > 2" class="hint"> 3. The country is: {{ randomCountry.name }}</p>
+      <p v-if="hints > 3" class="hint red-text">No more hints available. Use next flag button to skip.</p>
+      <!-- <p v-if="result==='correct'" class="result-correct"><font-awesome-icon :icon="['fas', 'check-circle']"/></p>
+      <p v-if="result==='incorrect'" class="result-wrong"><font-awesome-icon :icon="['fas', 'times-circle']"/></p> -->
     </section>
     <div id="counters">
       <div id="counters-container">
@@ -32,13 +40,32 @@ export default {
   props: ["randomCountry", "countriesRemaining", "correctAnswers", "result"],
   data () {
     return {
-
+      hints: 0
     }
   },
+
+  mounted() {
+    eventBus.$on('get-hint-pressed', () => {
+      this.hints += 1
+    })
+
+    eventBus.$on('change-flag-pressed', () => {
+      this.hints = 0
+    })
+
+    eventBus.$on('country-correct', () => {
+      this.hints = 0
+    })
+  },
+
   methods: {
     getRandomCountry (countriesRemaining) {
       eventBus.$emit('change-flag-pressed', countriesRemaining)
     },
+
+    getHint() {
+      this.hints += 1
+    }
   },
 
   components: {
@@ -63,14 +90,47 @@ export default {
   height: 20vh;
 }
 
-#flag-results {
+#hints-container {
   display: flex;
-  flex-flow: row;
-  justify-content: center;
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  font-size: medium;
+  overflow-y: auto;
+  /* padding: 5px; */
+  height: 20vh;
+  width: 20vw;
+}
+
+.hint {
+  padding: 0 0 0 0;
+  margin: 0;
+}
+
+.red-text {
+  color: red;
+}
+
+#hint-button {
+    margin-left: 10px;
+    border-radius: 5px;
+    text-align: center;
+    outline: none;
+    padding: 8px 10px;
+    background-color: #ffd30d;
+    border: #ebb810 solid 2px;
+}
+
+#hints-header-container {
+  display: flex;
+  flex-flow: row nowrap;
   align-items: center;
-  font-size: xx-large;
-  padding: 5px;
-  height: 1em;
+}
+
+#hints-header {
+  font-size: large;
+  /* padding: 0 0 1em 0; */
+  margin: 0;
 }
 
 .result-wrong {
