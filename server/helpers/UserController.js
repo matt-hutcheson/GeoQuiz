6bb7d1f7c9e3f190fd1afe93070a9fa1c73933e1
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2021-02-17 17:08:46 +0000

    added register user to user controller

---
+++ server/helpers/UserController.js
@@ -0,0 +1,24 @@
+const User = require("./UserModel.js");
+
+exports.registerNewUser = async (req, res) => {
+  try {
+    console.log(isUser);
+    if (isUser.length >= 1) {
+      return res.status(409).json({
+        message: "username already in use"
+      })
+    }
+    const user = new User({
+      name: req.body.name,
+      password: req.body.password,
+      results: req.body.results
+    })
+    let data = await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).json({ data, token });
+  } catch (err) {
+    res.status(400).json({ err: err });
+  }
+};
+exports.loginUser = async (req, res) => {};
+exports.getUserDetails = async (req, res) => {};
