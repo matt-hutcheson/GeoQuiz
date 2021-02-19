3c0331437cca3259c1174e7bc0283fd975d21cfd
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2021-02-19 15:48:46 +0000

    removed console.logs for use in use check

--- server/helpers/UserController.js
+++ server/helpers/UserController.js
@@ -4,8 +4,6 @@ exports.registerNewUser = async (req, res) => {
   try {
     const username = req.body.username;
     const isUser = await User.isUsernameInUse(username);
-    console.log(username);
-    console.log(isUser)
     if (isUser) {
       return res.status(409).json({
         message: "username already in use"
